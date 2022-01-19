c0e58271903d9377dd003b27f9a05fe967c54858
Jishankduy <96292344+Jishankduy@users.noreply.github.com>
2022-01-19 15:08:32 +0530

    Jume input System
    Movement and put your code there make a pull request to your own repository

--- 2D unity kit (unity)/Assets/New Assets/Scripts/PlayerControlller.cs
+++ 2D unity kit (unity)/Assets/New Assets/Scripts/PlayerControlller.cs
@@ -1,29 +1,73 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControlller : MonoBehaviour
 {
    public Animator animator;
+   public float speed;
+   public float Jump;
+   private Rigidbody2D rb2D;
 
-    // Start is called before the first frame update
-    void Start()
+    
+    private void Awake()
     {
-        
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
 
+        MoveCharacter(horizontal, vertical);
+        PlayMovementAnimation(horizontal, vertical);
+    }
+
+    private void MoveCharacter(float horizontal, float vertical){
+        Vector3 position = transform.position;
+        position.x = position.x + horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+        if(vertical > 0){
+           rb2D.AddForce(new Vector2(0f,Jump), ForceMode2D.Force); 
+        }
+    }
+
+    private void PlayMovementAnimation(float horizontal, float vertical) {
+        animator.SetFloat("Speed",Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
-        if (speed < 0) {
+        if (horizontal < 0) {
             scale.x = -1f * Mathf.Abs(scale.x);
-        } else if(speed > 0) {
+        } else if(horizontal > 0) {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+        //Jump
+       
+        if (Input.GetButtonDown("Jump")) {
+            animator.SetTrigger("jump");
+        };
+
+        //Crourch
+
+        if (Input.GetButtonDown("Fire1")) {
+            animator.SetTrigger("Crourch");
+        };
+
+        // // Run
+        // if (Input.GetButtonDown("Fire3 > ")) {
+        //     animator.SetBool("Run", true);
+        // }
+
+        // else (Input.GetButtonDown("Fire3"))
+        // {
+        //     animator.SetBool("Run", false);
+        // }
+        
+
     }
+    
+    
 }
